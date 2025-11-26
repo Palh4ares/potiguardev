e4468a879f91555cf3295143df74828bdcf64f71
Palh4ares <87243505+Palh4ares@users.noreply.github.com>
2025-11-26 16:22:11 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/potiguardev/",  // <- importante: o nome exato do repositório
+  base: "/potiguardev/",
 });
